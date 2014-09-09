1d20ab1a3cf2070f85c892d9375f972eca72c468
Ben Moore <b3nmoore@gmail.com>
2014-09-09 13:47:50 +0100

    updated sequencing cost up to April 2014

--- R/genome_cost.R
+++ R/genome_cost.R
@@ -1,10 +1,11 @@
-library(zoo)
-library(scales)
-setwd("~/other/graphapedia/")
+library("ggplot2")
+library("scales")
+library("reshape2")
+library("zoo")
 
 # from http://www.genome.gov/pages/der/sequencing_cost_apr13.xls
-seq <- read.table("data/sequencing_cost_apr13.csv", stringsAsFactors=F, header=T)
-seq$CostPerMb <- NULL
+seq <- read.table("data/sequencing_costs_apr2014.csv", stringsAsFactors=F, header=T, sep=",")
+seq$CostperMb <- NULL
 
 seq <- melt(seq, 1)
 seq$Date <- as.yearmon(seq$Date, format="%b-%y")
@@ -19,6 +20,6 @@ ggplot(seq, aes(x=as.Date(Date), y=value)) +
   scale_y_log10(limits=c(900,100000000),
                 breaks=c(1e3, 1e4, 1e5, 1e6, 1e7, 1e8),
                 labels=comma) +
-  ggtitle("Genome sequencing cost as estimated by NHGRI\n(September 2001 to April 2013)") +
+  ggtitle("Genome sequencing cost as estimated by NHGRI\n(September 2001 to April 2014)") +
   theme(plot.title=element_text(size=12))
 dev.off()
